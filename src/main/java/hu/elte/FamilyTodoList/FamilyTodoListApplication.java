d3f80945eb2ea9e87c281f21c8ac70ae48e897aa
Hunor Lakatos <lakatoshunor9716@gmail.com>
2019-09-20 12:16:33 +0200

    created project via Spring Initializr

---
+++ src/main/java/hu/elte/FamilyTodoList/FamilyTodoListApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.FamilyTodoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyTodoListApplication.class, args);
+	}
+
+}
