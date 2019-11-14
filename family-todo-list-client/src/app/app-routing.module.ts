35b2be15f6d78ad9e4fc157f0d5f002b8a29d9c6
Zoitom <mihaelle98@gmail.com>
2019-11-14 10:17:13 +0100

    init pages and primitive routing

--- family-todo-list-client/src/app/app-routing.module.ts
+++ family-todo-list-client/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { NewTaskPageComponent } from './new-task-page/new-task-page.component';
+import { EditTaskPageComponent } from './edit-task-page/edit-task-page.component';
+import { UserProfilePageComponent } from './user-profile-page/user-profile-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+	{ path: 'dashboard', component: DashboardPageComponent },
+	{ path: 'new', component: NewTaskPageComponent },
+	{ path: 'edit/:id', component: EditTaskPageComponent },
+	{ path: 'profile', component: UserProfilePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- family-todo-list-client/src/app/app.module.ts
+++ family-todo-list-client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { NewTaskPageComponent } from './new-task-page/new-task-page.component';
+import { EditTaskPageComponent } from './edit-task-page/edit-task-page.component';
+import { UserProfilePageComponent } from './user-profile-page/user-profile-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardPageComponent,
+    NewTaskPageComponent,
+    EditTaskPageComponent,
+    UserProfilePageComponent
   ],
   imports: [
     BrowserModule,
