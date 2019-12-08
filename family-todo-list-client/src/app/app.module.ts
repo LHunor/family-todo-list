060408a93557eb613fff276e649e2a15e0e78857
Zoitom <mihaelle98@gmail.com>
2019-12-08 18:25:56 +0100

    itt a megjelenites

--- family-todo-list-client/src/app/app.module.ts
+++ family-todo-list-client/src/app/app.module.ts
@@ -1,22 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MatToolbarModule, MatIconModule, MatButtonModule, MatFormFieldModule, MatButtonToggleModule, MatMenuModule, MatButtonToggleGroup } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { DescriptionComponent } from './description/description.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { GroupListComponent } from './group-list/group-list.component';
 import { TaskListComponent } from './task-list/task-list.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatToolbarModule, MatIconModule, MatButtonModule } from '@angular/material';
-
-import {FlexLayoutModule} from '@angular/flex-layout';
-
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { RoutingModule } from './routing/routing.module';
+import { StageFilterComponent } from './stage-filter/stage-filter.component';
 
 
 
@@ -27,16 +29,21 @@ import { RoutingModule } from './routing/routing.module';
     LoginComponent,
     GroupListComponent,
     TaskListComponent,
-    RegisterComponent
+    RegisterComponent,
+    StageFilterComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule, MatIconModule, MatButtonModule,
-    FlexLayoutModule,
     NgbModule,
-    RoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RoutingModule,
+    MatToolbarModule, MatIconModule, MatButtonModule, 
+    MatButtonToggleModule, MatFormFieldModule, MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- family-todo-list-client/src/app/task-list/task-list.component.ts
+++ family-todo-list-client/src/app/task-list/task-list.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from "../task";
+import { TaskService } from "../task.service";
 
 @Component({
-  selector: 'task-list',
+  selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent implements OnInit {
+  filteredTasks: Task[];
+  selectedStage: string;
+  selectedTask: Task;
 
-  constructor() { }
+  tasks: Task[] = [];
 
-  ngOnInit() {
+  constructor(private taskService: TaskService) { }
+
+  async ngOnInit(): Promise<void> {
+    this.selectedStage = '';
+    this.tasks = await this.taskService.getTasks();
+  	this.filter();
+  }
+
+  onFilterChange(stage: string) {
+  	this.selectedStage = stage;
+  	this.filter();
+  }
+
+  onSelectTask(task: Task) {
+  	this.selectedTask = task;
+  }
+
+  onDeleteClick(id: number) {
+    this.taskService.deleteTask(id)
+    .then(async () => {
+      this.selectedTask = null;
+      this.tasks = await this.taskService.getTasks();
+      this.filter();
+    })
+  }
+
+  private filter():void {
+  	this.filteredTasks = this.selectedStage === ''
+  	? this.tasks : this.tasks.filter(task => task.stage === this.selectedStage);
   }
 
 }
